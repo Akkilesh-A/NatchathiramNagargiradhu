e60f12948d9b40e3a2616111648945a79bd34d82
spacecentre <akkilalagar05@gmail.com>
2025-05-28 16:39:40 +0000

    fix: fix video imports

--- frontend/src/components/background-video.tsx
+++ frontend/src/components/background-video.tsx
@@ -7,7 +7,13 @@ const BackgroundVideo = () => {
     const [video,setVideo] = useState(1)
     useEffect(() => {
         const interval = setInterval(() => {
-            setVideo((prevVideo) => (prevVideo === 3 ? 4: prevVideo===2 ? 3 : prevVideo==1 ? 2 : 1));
+            // setVideo((prevVideo) => (prevVideo === 3 ? 4: prevVideo===2 ? 3 : prevVideo==1 ? 2 : 1));
+            setVideo((prevVideo) => {
+                if (prevVideo === 1) return 2;
+                // if (prevVideo === 2) return 3;
+                // if (prevVideo === 3) return 4;  
+                return 1; // Reset to 1 after reaching 4
+            })
         }, 3000);
 
         return () => clearInterval(interval);
@@ -20,12 +26,12 @@ const BackgroundVideo = () => {
         <video autoPlay muted loop className={` ${video ===2 ? "block" : "hidden"}`}>
             <source src={`/2.mp4`} type="video/mp4"/>
         </video>
-        <video autoPlay muted loop className={` ${video === 3 ? "block" : "hidden"}`}>
+        {/* <video autoPlay muted loop className={` ${video === 3 ? "block" : "hidden"}`}>
             <source src={`/3.mp4`} type="video/mp4"/>
         </video>
         <video autoPlay muted loop className={` ${video === 4 ? "block" : "hidden"}`}>
             <source src={`/4.mp4`} type="video/mp4"/>
-        </video>
+        </video> */}
     </div>
   )
 }
